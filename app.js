0b58257ff25423c1c2cc5ef799810a2af96799c1
Simon Situ <ssitu001@gmail.com>
2016-07-09 23:03:17 -0700

    more logic to figure out

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 $( document ).ready(function() {
   var defaultMin = 25;
+  var breakMins = 5;
   var userMinutes;
-  var minutes = defaultMin;
-  var seconds = minutes * 60;
+  // var minutes = defaultMin;
+  var defaultSeconds = defaultMin * 60;
+  var breakSeconds = breakMins * 60;
   var currentMinutes = 0;
   var currentSeconds = 0;
   var timer;
   var timerStarted = false;
 
-  var breakMins = 5;
   var isBreak = false;
   //When page loads reset timer to default time;
   reset();
 
   $('#test').on('click', function () {
-    breakTime();
+    console.log("testing");
+    $('#mainTimer').empty();
+    $('#mainTimer').html('<h1>'+breakMins + ' mins</h1>');
+    decrement();
   }); 
 
   $('#add').on('click', function() {
     console.log("user incremented");
     clearTimeout(timer);
     timerStarted = false;
-    userMinutes = minutes++;
-    userInput(minutes);
+    userMinutes = defaultMin++;
+    userInput(defaultMin);
   });
 
   $('#subtract').on('click', function() {
     console.log("user decremented");
     clearTimeout(timer);
     timerStarted = false;
-    userMinutes = minutes--;
-    userInput(minutes);
+    userMinutes = defaultMin--;
+    userInput(defaultMin);
   });
 
   $('#reset').on('click', function() {
@@ -40,7 +44,7 @@ $( document ).ready(function() {
   $('#mainTimer').on('click', function() {
     if (!timerStarted) {
       console.log("timer started");
-      decrement();  
+      decrement(work, defaultSeconds);  
       timerStarted = !timerStarted;    
     } else {
       console.log("stop");
@@ -51,7 +55,7 @@ $( document ).ready(function() {
   });
 
   //work Timer
-  function decrement(type, minutes) {
+  function decrement(type, seconds) {
     //TODO: determine type of timer and minutes (work or break)
     currentMinutes = Math.floor(seconds / 60);
     currentSeconds = seconds % 60;
@@ -69,9 +73,12 @@ $( document ).ready(function() {
     } 
     
     if(seconds === 0) {
-      console.log('timez up!');
+      isBreak = !isBreak;
+      console.log('timez up!', isBreak);
       //call break
-      breakTime();
+    // $('#mainTimer').empty();
+    // $('#mainTimer').html('<h1>'+breakMins + ' mins</h1>');
+    // decrement();
     }
 
   }
@@ -80,26 +87,27 @@ $( document ).ready(function() {
   function reset() {
     console.log("timer reset")
     timerStarted = false;
-    minutes = defaultMin;
-    seconds = minutes * 60;
+    var minutes = defaultMin;
+    var seconds = minutes * 60;
+    var breakminss = breakMins;
     clearTimeout(timer);
-    $('#mainTimer').html('<div><h4>Time</h4></div><h1>'+minutes + ':' + '00</h1>');
-    $('#breakTimer').html('<div><h4>Break Time</h4></div><h1>'+breakMins + ':' + '00</h1>');
-    $('#workTimer').html('<div><h4>Work Time</h4></div><h1>'+minutes + ':' + '00</h1>');
+    $('#mainTimer').html('<div><h4>Tap To Start Time</h4></div><h1>'+minutes + ' mins</h1>');
+    $('#breakTimer').html('<div><h4>Break Time</h4></div><h1>'+breakminss + ' mins</h1>');
+    $('#workTimer').html('<div><h4>Work Time</h4></div><h1>'+minutes + ' mins</h1>');
   }
 
   //user can increment/decrement
   function userInput(min) {
-    minutes = min;
-    seconds = min * 60;
-    $('#mainTimer').html('<h1>'+min + ':' + '00</h1>');   
+    defaultMin = min;
+    defaultSeconds = min * 60;
+    $('#mainTimer').html('<div><h4>Okay Okay! Tap To Start</h4></div><h1>'+min  + ' mins</h1>');
+    $('#workTimer').html('<div><h4>Work Time</h4></div><h1>'+min + ' mins</h1>');   
   }
   
   //break logic
   function breakTime() {
     console.log('break time');
-    $('#mainTimer').empty();
-    $('#mainTimer').html('<h1>'+breakMins + ':' + '00</h1>');
+  
   }
   
 });
